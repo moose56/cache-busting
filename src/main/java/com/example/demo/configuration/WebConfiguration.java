dcd1dde2dbdaf2e88f09f47d8826f7ecab974519
David Madden <moose56@gmail.com>
2019-06-01 12:45:26 +0100

    Initial commit

---
+++ src/main/java/com/example/demo/configuration/WebConfiguration.java
@@ -0,0 +1,22 @@
+package com.example.demo.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.resource.VersionResourceResolver;
+
+//@Configuration
+public class WebConfiguration implements WebMvcConfigurer
+{
+    private static final String[] CLASSPATH_RESOURCE_LOCATIONS = {
+            "classpath:/META-INF/resources/", "classpath:/resources/",
+            "classpath:/static/", "classpath:/public/", "classpath:/custom/"};
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry)
+    {
+        registry.addResourceHandler("/**").addResourceLocations(CLASSPATH_RESOURCE_LOCATIONS)
+                .resourceChain(true)
+                .addResolver(new VersionResourceResolver().addContentVersionStrategy("/**/*.js", "/**/*.css"));
+    }
+}
